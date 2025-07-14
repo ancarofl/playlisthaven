03a64361c6b1d6407f5cd910e11fea5acfdf86ad
Anca <negulescuanca11@gmail.com>
2025-07-14 23:24:46 +0200

    PH-9: remove some placeholders

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -3,21 +3,15 @@
 import Link from "next/link";
 import { ThemeToggle } from "./theme-toggle";
 import { MobileNav } from "./mobile-nav";
-import { Copy, GitBranch, Layers, Home, Settings, Music, User, Star, Heart } from "lucide-react";
+import { Copy, Layers, Settings, Music, Heart } from "lucide-react";
 
 // TODO: Yes update
 const links = [
 	{ href: "/clone", label: "Clone Playlist", Icon: Copy }, //  TODO: Name?
 	{ href: "/placeholder1", label: "Placeholder 1", Icon: Heart },
-	{ href: "/placeholder2", label: "Placeholder 2", Icon: GitBranch },
-	{ href: "/placeholder3", label: "Placeholder 3", Icon: Layers },
-	{ href: "/placeholder4", label: "Placeholder 4", Icon: Home },
-	{ href: "/placeholder5", label: "Placeholder 5", Icon: Settings },
-	{ href: "/placeholder6", label: "Placeholder 6", Icon: Music },
-	{ href: "/placeholder7", label: "Placeholder 7", Icon: User },
-	{ href: "/placeholder8", label: "Placeholder 8", Icon: User },
-	{ href: "/placeholder9", label: "Placeholder 9", Icon: User },
-	{ href: "/placeholder810", label: "XDDD", Icon: Star },
+	{ href: "/placeholder2", label: "Placeholder 2", Icon: Layers },
+	{ href: "/placeholder3", label: "Placeholder 3", Icon: Settings },
+	{ href: "/placeholder4", label: "Placeholder 4", Icon: Music },
 ];
 
 export function NavBar() {
