807c86ba3b7397854aac5c002b3d251e835faa91
Anca <negulescuanca11@gmail.com>
2025-07-17 23:37:07 +0200

    Chore: constant name

--- src/app/middleware/guest-session.ts
+++ src/app/middleware/guest-session.ts
@@ -1,10 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function guestSessionMiddleware(req: NextRequest, res: NextResponse): NextResponse {
-	const cookieName = "playlisthaven_guest_session";
+	const COOKIE_NAME = "playlisthaven_guest_session";
 
 	// Check if the guest session cookie already exists
-	const existingCookie = req.cookies.get(cookieName);
+	const existingCookie = req.cookies.get(COOKIE_NAME);
 
 	if (!existingCookie) {
 		// If not, generate a new session ID
@@ -12,7 +12,7 @@ export function guestSessionMiddleware(req: NextRequest, res: NextResponse): Nex
 
 		// Set the guest session cookie with appropriate flags
 		res.cookies.set({
-			name: cookieName,
+			name: COOKIE_NAME,
 			value: guestSessionId,
 			maxAge: 60 * 60 * 24, // s * m * h. 1 day (in seconds). Better option than expires, where you set an exact expiration date/time
 			path: "/", // Cookie applies to entire site. This is also the default, but typing it for clarity
