d8475eb087defec839e172a4d363b406f7310035
Anca <negulescuanca11@gmail.com>
2025-07-27 13:03:01 +0200

    PH-30: decouple callback controller from HTTP

--- src/controllers/oauth/callback-controller.ts
+++ src/controllers/oauth/callback-controller.ts
@@ -1,19 +1,16 @@
 import { BadRequestError, UnsupportedProviderError } from "@/helpers/errors";
 import { oauthServices } from "@/services/oauth";
-import { OauthService } from "@/types/oauth";
+import { OauthCallbackParams, OauthService } from "@/types/oauth";
 
-export async function oauthCallbackController(provider: string, searchParams: URLSearchParams): Promise<void> {
+export async function oauthCallbackController(provider: string, params: OauthCallbackParams): Promise<void> {
+	const { code, sessionId } = params;
+	if (!code || !sessionId) {
+		throw new BadRequestError("Missing code or session.");
+	}
+
 	const service: OauthService | undefined = oauthServices[provider];
 	if (!service) {
 		throw new UnsupportedProviderError(`OAuth provider '${provider}' is not supported.`);
-	}
-
-	// TODO: Extract to helper if it'll be reused, wait and see
-	const code = searchParams.get("code");
-	const sessionId = searchParams.get("state");
-
-	if (!code || !sessionId) {
-		throw new BadRequestError("Missing code or session.");
 	}
 
 	await service.exchangeCodeForTokens({ code, sessionId });
