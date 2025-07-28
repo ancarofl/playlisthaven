06c343a80a5e068c3c4688828a075557adc9f2c0
Anca <negulescuanca11@gmail.com>
2025-07-28 23:54:54 +0200

    PH-40: add test coverage for fun

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,5 +9,13 @@ export default defineConfig({
 			"@": path.resolve(__dirname, "src"),
 		},
 		setupFiles: "./src/setup-tests.ts",
+		coverage: {
+			provider: "v8", // or "istanbul"
+			reporter: ["text", "html"], // show in terminal and generate HTML report
+			reportsDirectory: "./coverage",
+			all: true, // include files even if not tested
+			include: ["src/**/*.{ts,tsx,js,jsx}"], // path to source files
+			exclude: ["src/generated/prisma/**", "node_modules/**", "tests/**"],
+		},
 	},
 });
